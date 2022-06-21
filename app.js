6ef9b5f181e19c4813561183096ebba064a2cc0d
GSvenny <gabrielswagner7@gmail.com>
2022-06-20 19:42:38 -0500

    created additional js file

--- app.js
+++ app.js
@@ -1,14 +1,7 @@
+const fs = require('fs');
+
 const profileDataArgs = process.argv.slice(2);
 
-const printProfileData = profileDataArr => {
-  for (let i = 0; i < profileDataArr.length; i += 1) {
-    console.log(profileDataArr[i]);
-  }
+const [name, github] = profileDataArgs;
 
-  console.log('================');
-
-  // Is the same as this...
-  profileDataArr.forEach(profileItem => console.log(profileItem));
-};
-
-printProfileData(profileDataArgs);
+const generatePage = require('./src/page-template.js');
