c3423b467d5ce124eabc321af20cb95fd0c59fcc
GSvenny <gabrielswagner7@gmail.com>
2022-06-22 22:24:10 -0500

    added inquirer

--- app.js
+++ app.js
@@ -1,14 +1,54 @@
-const profileDataArgs = process.argv.slice(2);
+const inquirer = require('inquirer');
+// const fs = require('fs');
+// const generatePage = require('./src/page-template');
 
-const printProfileData = profileDataArr => {
-  for (let i = 0; i < profileDataArr.length; i += 1) {
-    console.log(profileDataArr[i]);
-  }
+// const pageHTML = generatePage(name, github);
 
-  console.log('================');
+// fs.writeFile('./index.html', pageHTML, err => {
+//   if (err) throw err;
 
-  // Is the same as this...
-  profileDataArr.forEach(profileItem => console.log(profileItem));
+//   console.log('Portfolio complete! Check out index.html to see the output!');
+// });
+
+const promptProject = () => {
+  console.log(`
+=================
+Add a New Project
+=================
+`);
+  return inquirer.prompt([
+    {
+      type: 'input',
+      name: 'name',
+      message: 'What is the name of your project?'
+    },
+    {
+      type: 'input',
+      name: 'description',
+      message: 'Provide a description of the project (Required)'
+    },
+    {
+      type: 'checkbox',
+      name: 'languages',
+      message: 'What did you build this project with? (Check all that apply)',
+      choices: ['JavaScript', 'HTML', 'CSS', 'ES6', 'jQuery', 'Bootstrap', 'Node']
+    },
+    {
+      type: 'input',
+      name: 'link',
+      message: 'Enter the GitHub link to your project. (Required)'
+    },
+    {
+      type: 'confirm',
+      name: 'feature',
+      message: 'Would you like to feature this project?',
+      default: false
+    },
+    {
+      type: 'confirm',
+      name: 'confirmAddProject',
+      message: 'Would you like to enter another project?',
+      default: false
+    }
+  ]);
 };
-
-printProfileData(profileDataArgs);
